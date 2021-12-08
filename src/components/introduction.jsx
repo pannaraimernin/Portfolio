4685418b75c9a712a488799f6a1556d88b10e40a
pannaraimernin <panna.tech311@gmail.com>
2021-12-08 12:53:29 -0600

    update code

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/about.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
